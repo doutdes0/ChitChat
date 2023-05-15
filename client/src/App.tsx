233ef83c11ac5c5161cf17cda7dce0e4f52722db
Artem Gromakov <pandawithmelon@gmail.com>
2023-05-15 17:01:32 +0300

    Add browserRouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
-function App() {
-  return <></>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import SignUp from './pages/Sign-up';
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route
+        path="sign-up"
+        element={<SignUp />}
+      />
+      <Route
+        path="login"
+        element={<Login />}
+      />
+      <Route
+        path="/"
+        element={<Chat />}
+      />
+    </Routes>
+  );
+};
 
 export default App;
