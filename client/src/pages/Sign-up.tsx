b1347c084181479daa7860b36eb805dba50bc122
Artem Gromakov <pandawithmelon@gmail.com>
2023-05-18 21:09:45 +0300

    Create styled container

--- client/src/pages/Sign-up.tsx
+++ client/src/pages/Sign-up.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
+import logo from '../assets/logo.png';
 
 const SignUp: React.FC = () => {
   const [input, setInput] = useState({
@@ -9,19 +12,25 @@ const SignUp: React.FC = () => {
   });
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    setInput({ [name]: value, ...input });
+    setInput({ ...input, [name]: value });
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
   };
   return (
     <>
-      <div className="card-form">
-        <span className="icon">
-          <i className="fa-solid fa-dove fa-beat-fade fa-4x"></i>
-        </span>
-        <h2>ChitChat</h2>
-        <form className="form">
-          <div className="form-item">
+      <AuthContainer>
+        <div className="form-wrapper">
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className="logo-wrapper">
+              <img
+                src={logo}
+                alt="logo"
+              />
+              <h2>CHITCHAT</h2>
+            </div>
+
             <input
-              className="input"
               type="text"
               placeholder="Username"
               name="username"
@@ -29,50 +38,82 @@ const SignUp: React.FC = () => {
               onChange={(e) => handleChange(e)}
               autoFocus
             />
-          </div>
-          <div className="form-item">
+
             <input
-              className="input"
               type="email"
               placeholder="E-mail address"
               name="email"
               value={input.email}
               onChange={(e) => handleChange(e)}
             />
-          </div>
-          <div className="form-item">
+
             <input
-              className="input"
               type="password"
               placeholder="Password"
               name="password"
               value={input.password}
               onChange={(e) => handleChange(e)}
             />
-          </div>
-          <div className="form-item">
+
             <input
-              className="input"
               type="password"
               placeholder="Confirm password"
               name="confirmPassword"
               value={input.confirmPassword}
               onChange={(e) => handleChange(e)}
             />
-          </div>
-          <div className="form-item btn-wrapper">
-            <button
-              className="btn"
-              type="submit"
-            >
-              SIGN UP
-            </button>
-          </div>
-        </form>
-        <p>ⓒBark, bork, bjork</p>
-      </div>
+            <button type="submit">SUBMIT</button>
+            <span>
+              Already have an account? <Link to="/login">LOGIN</Link>
+            </span>
+          </form>
+        </div>
+      </AuthContainer>
     </>
   );
 };
 
 export default SignUp;
+
+const AuthContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background: var(--black);
+  .logo-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    img {
+      height: 5rem;
+    }
+    h2 {
+      color: #ffffff;
+    }
+  }
+  .form-wrapper {
+    background-image: url('/src/assets/form-bg.svg');
+    /* background by SVGBackgrounds.com */
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 3rem;
+    box-shadow: 0px 0px 7px 5px rgba(29, 117, 94, 0.75) inset;
+    opacity: 0.999; //keeps backdrop-blur + border-radius from bluring bg inside wrapper
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 3rem 5rem;
+    border-radius: 3rem;
+    background: rgba(0, 0, 0, 0.25);
+    -weblit-backdrop-filter: blur(8px);
+    backdrop-filter: blur(8px);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+  }
+`;
