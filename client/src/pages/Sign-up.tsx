071dfd42bf74592231b5d120a2a761f519db3d56
Artem Gromakov <pandawithmelon@gmail.com>
2023-05-19 23:26:04 +0300

    Finish styling sign-up page

--- client/src/pages/Sign-up.tsx
+++ client/src/pages/Sign-up.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
-import logo from '../assets/logo.png';
+import spiral from '../assets/logo-spiral.png';
+import chatBubble from '../assets/chatbubble.png';
 
 const SignUp: React.FC = () => {
   const [input, setInput] = useState({
@@ -20,54 +21,57 @@ const SignUp: React.FC = () => {
   return (
     <>
       <AuthContainer>
-        <div className="form-wrapper">
-          <form onSubmit={(e) => handleSubmit(e)}>
-            <div className="logo-wrapper">
-              <img
-                src={logo}
-                alt="logo"
-              />
-              <h2>CHITCHAT</h2>
-            </div>
-
-            <input
-              type="text"
-              placeholder="Username"
-              name="username"
-              value={input.username}
-              onChange={(e) => handleChange(e)}
-              autoFocus
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="logo-wrapper">
+            <img
+              src={spiral}
+              alt="spiral"
             />
-
-            <input
-              type="email"
-              placeholder="E-mail address"
-              name="email"
-              value={input.email}
-              onChange={(e) => handleChange(e)}
+            <img
+              className="chat-icon"
+              src={chatBubble}
+              alt="chat-icon"
             />
+            <h1>ChitChAt</h1>
+          </div>
 
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={input.password}
-              onChange={(e) => handleChange(e)}
-            />
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            value={input.username}
+            onChange={(e) => handleChange(e)}
+            autoFocus
+          />
 
-            <input
-              type="password"
-              placeholder="Confirm password"
-              name="confirmPassword"
-              value={input.confirmPassword}
-              onChange={(e) => handleChange(e)}
-            />
-            <button type="submit">SUBMIT</button>
-            <span>
-              Already have an account? <Link to="/login">LOGIN</Link>
-            </span>
-          </form>
-        </div>
+          <input
+            type="email"
+            placeholder="E-mail address"
+            name="email"
+            value={input.email}
+            onChange={(e) => handleChange(e)}
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={input.password}
+            onChange={(e) => handleChange(e)}
+          />
+
+          <input
+            type="password"
+            placeholder="Confirm password"
+            name="confirmPassword"
+            value={input.confirmPassword}
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">CREATE ACCOUNT</button>
+          <span>
+            Already have an account? <Link to="/login">LOGIN</Link>
+          </span>
+        </form>
       </AuthContainer>
     </>
   );
@@ -82,38 +86,83 @@ const AuthContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
-  background: var(--black);
+  background-image: url('/src/assets/form-bg.svg');
+  /* background by SVGBackgrounds.com */
+  background-repeat: no-repeat;
+  background-size: cover;
   .logo-wrapper {
     display: flex;
+    position: relative;
     justify-content: center;
     align-items: center;
     gap: 1rem;
-    img {
-      height: 5rem;
+    &:hover > img:first-of-type {
+      rotate: 360deg;
     }
-    h2 {
+    img:first-of-type {
+      height: 6rem;
+      transition: rotate 3s;
+    }
+    h1 {
+      font-family: var(--ms-family);
       color: #ffffff;
+      cursor: default;
     }
-  }
-  .form-wrapper {
-    background-image: url('/src/assets/form-bg.svg');
-    /* background by SVGBackgrounds.com */
-    background-repeat: no-repeat;
-    background-size: cover;
-    border-radius: 3rem;
-    box-shadow: 0px 0px 7px 5px rgba(29, 117, 94, 0.75) inset;
-    opacity: 0.999; //keeps backdrop-blur + border-radius from bluring bg inside wrapper
+    .chat-icon {
+      position: absolute;
+      height: 2rem;
+      left: 38px;
+    }
   }
   form {
     display: flex;
     flex-direction: column;
-    gap: 2rem;
-    padding: 3rem 5rem;
+    gap: 1.5rem;
+    padding: 2.5rem 4.5rem;
     border-radius: 3rem;
-    background: rgba(0, 0, 0, 0.25);
-    -weblit-backdrop-filter: blur(8px);
-    backdrop-filter: blur(8px);
+    background: rgba(255, 255, 255, 0.2);
+    backdrop-filter: blur(110px);
     border: 1px solid rgba(255, 255, 255, 0.3);
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.3);
+    input {
+      width: 100%;
+      color: #ffffff;
+      border: 1px solid #ffffff;
+      padding: 0.5rem 0 0.5rem 1rem;
+      border-radius: 0.5rem;
+      outline: none;
+      background: transparent !important;
+      transition: all 0.5s;
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.8);
+        font-weight: 100;
+      }
+      &:focus {
+        border: 1px solid var(--light-green);
+      }
+    }
+    button {
+      background-color: var(--light-green);
+      color: #ffffff;
+      padding: 0.8rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.5rem;
+      transition: all 0.5s;
+      &:hover {
+        background-color: #ffffff;
+        color: var(--light-green);
+      }
+    }
+    span {
+      color: #ffffff;
+      text-align: center;
+      a {
+        color: var(--blue);
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
   }
 `;
