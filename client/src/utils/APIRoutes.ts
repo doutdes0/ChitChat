658a9c748a2a5674da6ff3e40a782f90a6a0bef6
Artem Gromakov <pandawithmelon@gmail.com>
2023-05-31 23:21:07 +0300

    Create axios instance, add API routes

---
+++ client/src/utils/APIRoutes.ts
@@ -0,0 +1,10 @@
+export enum APIRoutes {
+  HOST = 'http://localhost:3001',
+  LOGIN = '/api/auth/login',
+  LOGOUT = '/api/auth/logout',
+  SIGN_UP = '/api/auth/signup',
+  ALL_USERS = '/api/auth/allusers',
+  SET_AVATAR = '/api/auth/setavatar',
+  SEND_MESSAGE = '/api/messages/addmsg',
+  RECEIVE_MESSAGE = '/api/messages/getmsg',
+}
