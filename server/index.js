601c0d978bbd02034748169751b575782aca27b7
Artem Gromakov <pandawithmelon@gmail.com>
2023-06-06 23:07:12 +0300

    Add authrouter

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const authRouter = require('./routes/auth');
 require('dotenv').config();
 
 const app = express();
@@ -16,6 +17,8 @@ mongoose
   })
   .then(() => console.log('DB connected'))
   .catch((e) => console.log(`Connection failed: ${e.message}`));
+
+app.use('/API/auth', authRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server's running on port ${PORT}`));
