3f7c2226929551bc54b627b2d2905535a7723140
Artem Gromakov <pandawithmelon@gmail.com>
2023-06-07 19:08:55 +0300

    Change routes

--- client/src/utils/APIRoutes.ts
+++ client/src/utils/APIRoutes.ts
@@ -1,10 +1,10 @@
 export enum APIRoutes {
   HOST = 'http://localhost:3001',
-  LOGIN = '/api/auth/login',
-  LOGOUT = '/api/auth/logout',
-  SIGN_UP = '/api/auth/signup',
-  ALL_USERS = '/api/auth/allusers',
-  SET_AVATAR = '/api/auth/setavatar',
-  SEND_MESSAGE = '/api/messages/addmsg',
-  RECEIVE_MESSAGE = '/api/messages/getmsg',
+  LOGIN = '/API/auth/login',
+  LOGOUT = '/API/auth/logout',
+  SIGN_UP = '/API/auth/signup',
+  ALL_USERS = '/API/auth/allusers',
+  SET_AVATAR = '/API/auth/setavatar',
+  SEND_MESSAGE = '/API/messages/addmsg',
+  RECEIVE_MESSAGE = '/API/messages/getmsg',
 }
