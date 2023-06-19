bdf4a6482fdc9c7b9ce094fb3acc51cc1453676f
Artem Gromakov <pandawithmelon@gmail.com>
2023-06-19 21:53:21 +0300

    Complete login controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../model/userModel');
+const jwt = require('jsonwebtoken');
 
 const signup = async (req, res, next) => {
   try {
@@ -10,7 +11,7 @@ const signup = async (req, res, next) => {
     if (emailExists) return res.status(401).json({ msg: 'User with this email already exists' });
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({ username, email, password: hashedPassword });
-    return res.status(200).send();
+    res.sendStatus(200);
   } catch (e) {
     next(e);
   }
@@ -20,11 +21,30 @@ const login = async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    if (!user) return res.json({ msg: 'Incorrect username or password', status: 401 });
+    if (!user) return res.status(401).json({ msg: 'Incorrect username or password' });
     const isPwValid = await bcrypt.compare(password, user.password);
-    if (!isPwValid) return res.json({ msg: 'Incorrect username or password', status: 401 });
+    if (!isPwValid) return res.status(401).json({ msg: 'Incorrect username or password' });
+
+    const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '30s',
+    });
+    const refreshToken = jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: '1d',
+    });
+    await User.updateOne({ username }, { refreshToken });
+    user._id = user._id.toString();
+    user.accessToken = accessToken;
     delete user.password;
-    return res.json({ user, status: 200 });
+    delete user.email;
+    delete user.isAvatarSet;
+    delete user.refreshToken;
+    res.cookie('jwt', refreshToken, {
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({ user });
   } catch (e) {
     next(e);
   }
