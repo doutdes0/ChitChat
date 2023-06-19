481af70ae36132b8a3a3f65b1c388c4e016839ef
Artem Gromakov <pandawithmelon@gmail.com>
2023-06-19 21:56:47 +0300

    Code cleanup

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  if (!authHeader) return res.status(401);
+  if (!authHeader) return res.sendStatus(401);
   console.log(authHeader); // Bearer token
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
