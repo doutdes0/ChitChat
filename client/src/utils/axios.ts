2f443b593c9b470f18c2368ace1a4ff6d33dd43a
Artem Gromakov <pandawithmelon@gmail.com>
2023-06-28 23:42:53 +0300

    Change axios into public and private instances, add interceptors to handle expired access token

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,9 +1,49 @@
 import axios from 'axios';
 import { APIRoutes } from './APIRoutes';
+import { useAppSelector } from '../hooks/useRedux';
+import useRefreshToken from '../hooks/useRefreshToken';
 
-export default axios.create({
+export const axiosPublic = axios.create({
   baseURL: APIRoutes.HOST,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+export const axiosPrivate = axios.create({
+  baseURL: APIRoutes.HOST,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+axiosPrivate.interceptors.request.use(
+  (config) => {
+    const token = useAppSelector((state) => state.auth.accessToken);
+    if (token) {
+      config.headers['x-access-token'] = token;
+    }
+    return config;
+  },
+  (e) => Promise.reject(e)
+);
+
+axiosPrivate.interceptors.response.use(
+  (res) => res,
+  async (e) => {
+    const prevReq = e.config;
+    if (e.response.status === 401 && !prevReq._retry) {
+      prevReq._retry = true;
+
+      try {
+        useRefreshToken();
+        return axiosPrivate(prevReq);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(e);
+  }
+);
