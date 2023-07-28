b4989ef40cb5f9946d8d0f4f667544e22d42cb83
Artem Gromakov <pandawithmelon@gmail.com>
2023-07-28 14:45:12 +0300

    Add user slice to state

--- client/src/redux/reducers/usersSlice.ts
+++ client/src/redux/reducers/usersSlice.ts
@@ -1,13 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getAllUsers } from '../thunks';
 
-const initialState = {
-  id: '',
+export type UsersState = {
+  list: {
+    _id: string;
+    avatar: string;
+    username: string;
+  }[];
 };
+
+const initialState: UsersState = { list: [] };
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllUsers.fulfilled, (state, action) => {
+      state.list = action.payload;
+    });
+  },
 });
 
 export default usersSlice.reducer;
