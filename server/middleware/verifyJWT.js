2d133168c75d0be03e92a913e29606aecbcd243e
Artem Gromakov <pandawithmelon@gmail.com>
2023-08-04 17:58:13 +0300

    Add extra json responses to statuses, fix token retrieval

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers['x-access-token'];
-  if (!authHeader) return res.sendStatus(401);
-  console.log(authHeader); // Access token
-  const token = authHeader.split(' ')[1];
+  const token = req.headers['x-access-token'];
+  if (!token) return res.status(401).json({ msg: 'verifyJWT: No authorization header' });
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); //invalid token
+    if (err) {
+      return res.status(403).json({ msg: 'verifyJWT: Token invalid or expired' });
+    }
     req.user = decoded.username;
     next();
   });
