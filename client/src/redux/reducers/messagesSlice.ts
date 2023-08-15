437a4a6aa81394fa80390bf1ee17a8ccaa17b846
Artem Gromakov <pandawithmelon@gmail.com>
2023-08-15 20:32:53 +0300

    Develop messagesSlice

--- client/src/redux/reducers/messagesSlice.ts
+++ client/src/redux/reducers/messagesSlice.ts
@@ -1,13 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { getAllMsgs } from "../thunks";
 
-const initialState = {
-  id: '',
+type Message = {
+  isFromSelf: boolean;
+  msg: string;
 };
 
+export type Payload_Msg = {
+  userID: string;
+  msg: string;
+};
+
+export interface MessagesState {
+  [userID: string]: Message[];
+}
+const initialState: MessagesState = {};
+
 const messagesSlice = createSlice({
-  name: 'messages',
+  name: "messages",
   initialState,
-  reducers: {},
+  reducers: {
+    receive_msg: (state, action: PayloadAction<Payload_Msg>) => {
+      const { userID, msg } = action.payload;
+      state[userID].push({ isFromSelf: false, msg });
+    },
+    send_msg_io: (state, action: PayloadAction<Payload_Msg>) => {
+      const { userID, msg } = action.payload;
+      state[userID].push({ isFromSelf: true, msg });
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      getAllMsgs.fulfilled,
+      (state, action: PayloadAction<MessagesState>) => {
+        const messages = action.payload;
+        const userID = Object.keys(messages)[0];
+        state[userID] = messages.userID;
+      }
+    );
+  },
 });
 
 export default messagesSlice.reducer;
+export const { receive_msg, send_msg_io } = messagesSlice.actions;
