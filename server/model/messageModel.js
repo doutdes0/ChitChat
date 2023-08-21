e662c9bead914d622e442f9f3b5e5537ff7a2e02
Artem Gromakov <pandawithmelon@gmail.com>
2023-08-21 21:10:50 +0300

    Add timestamps

--- server/model/messageModel.js
+++ server/model/messageModel.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const messageSchema = mongoose.Schema({
-  message: {
-    type: String,
-    required: true,
+const messageSchema = mongoose.Schema(
+  {
+    message: {
+      type: String,
+      required: true,
+    },
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    users: [mongoose.Schema.Types.ObjectId],
   },
-  from: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  to: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
-const Message = mongoose.model('Messages', messageSchema);
+const Message = mongoose.model("Messages", messageSchema);
 module.exports = Message;
