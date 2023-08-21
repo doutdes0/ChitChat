0d2aff4553b90eb4cce83e3f8c8fd62b4a831859
Artem Gromakov <pandawithmelon@gmail.com>
2023-08-21 21:45:27 +0300

    Add allMessages and addMsg routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,13 @@
-const router = require('express').Router();
-const { setAvatar, getAllUsers } = require('../controllers/userController');
+const router = require("express").Router();
+const { setAvatar, getAllUsers } = require("../controllers/userController");
+const {
+  getAllMessages,
+  addMessage,
+} = require("../controllers/messageController");
 
-router.get('/allusers', getAllUsers);
-router.post('/setavatar/:id', setAvatar);
+router.get("/allusers", getAllUsers);
+router.post("/setavatar/:id", setAvatar);
+router.get("/allMsgs", getAllMessages);
+router.post("/addMsg", addMessage);
 
 module.exports = router;
