40d74a82a48d49ac960377f2a70cec309e3127e7
Artem Gromakov <pandawithmelon@gmail.com>
2023-08-25 21:56:41 +0300

    Tweak box shadow

--- client/src/components/ChatHeader.tsx
+++ client/src/components/ChatHeader.tsx
@@ -1,7 +1,7 @@
-import { selectedChat } from '../pages/Chat';
-import parse from 'html-react-parser';
-import { styled } from 'styled-components';
-import defIcon from '../assets/defIcon.png'; 
+import { selectedChat } from "../pages/Chat";
+import parse from "html-react-parser";
+import { styled } from "styled-components";
+import defIcon from "../assets/defIcon.png";
 
 type Props = {
   contact: selectedChat;
@@ -18,13 +18,14 @@ const ChatHeader: React.FC<Props> = ({ contact }) => {
 export default ChatHeader;
 
 const Container = styled.div`
-box-shadow: 0 1em 1em -1em hsla(0, 0%, 0%, 0.25);
-display: flex;
-align-items: center;
-justify-content: flex-start;
-padding: 2rem 0 2rem 1rem;
-gap: 1rem;
-svg, img {
-      width: 2rem;
-    }
+  box-shadow: 0 1em 3em -2em hsla(0, 0%, 0%, 0.25);
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 2rem 0 2rem 1rem;
+  gap: 1rem;
+  svg,
+  img {
+    width: 2rem;
+  }
 `;
