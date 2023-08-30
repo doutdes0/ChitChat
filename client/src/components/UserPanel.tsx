6bc1ad5df1ff7beb3ccb836bc7fc68da2edb8b9e
Artem Gromakov <pandawithmelon@gmail.com>
2023-08-30 21:43:20 +0300

    Add button panel for changeAvatar and logout

--- client/src/components/UserPanel.tsx
+++ client/src/components/UserPanel.tsx
@@ -1,27 +1,53 @@
-import { styled } from 'styled-components';
-import { useAppSelector, useAppDispatch } from '../hooks/useRedux';
-import parse from 'html-react-parser';
-import Contacts from './Contacts';
-import { selectedChat } from '../pages/Chat';
+import { styled } from "styled-components";
+import parse from "html-react-parser";
+import Contacts from "./Contacts";
+import { selectedChat } from "../pages/Chat";
+import lgicon from "../assets/logout-icon.svg";
+import avaticon from "../assets/changeAvatar-icon.svg";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../hooks/useRedux";
+import { logOut } from "../redux/reducers/authSlice";
 
 type Props = {
   username: string;
   avatar: string;
-  id: string;
   setChat: React.Dispatch<React.SetStateAction<selectedChat | null>>;
+  selectedChatID: string | undefined;
 };
 
-const UserPanel: React.FC<Props> = ({ username, avatar, id, setChat }) => {
+const UserPanel: React.FC<Props> = ({
+  username,
+  avatar,
+  setChat,
+  selectedChatID,
+}) => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const handleLogout = () => {
+    sessionStorage.removeItem("user");
+    dispatch(logOut());
+  };
   return (
     <Container>
       <div className="user-wrapper">
         {parse(avatar)}
         <p>{username}</p>
       </div>
-      <Contacts
-        id={id}
-        setChat={setChat}
-      />
+      <Contacts setChat={setChat} selectedChatID={selectedChatID} />
+      <div className="button-panel">
+        <img
+          onClick={() => navigate("setAvatar", { state: true })}
+          title="Change avatar"
+          src={avaticon}
+          alt="change_avatar-icon"
+        />
+        <img
+          onClick={() => handleLogout()}
+          title="Logout"
+          src={lgicon}
+          alt="logout-icon"
+        />
+      </div>
     </Container>
   );
 };
@@ -29,14 +55,12 @@ const UserPanel: React.FC<Props> = ({ username, avatar, id, setChat }) => {
 export default UserPanel;
 
 const Container = styled.div`
-  grid-column-start:2;
+  grid-column-start: 2;
   grid-column-end: 3;
-  grid-row-start:2;
-  grid-row-end:3;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  display: grid;
+  grid-template-rows: [first] 17% [row1] auto [row2] 10% [last];
   border-radius: 20px;
   background-color: var(--white_op90);
   border: 1px solid var(--white_op90);
@@ -47,9 +71,32 @@ const Container = styled.div`
     justify-content: start;
     padding: 1rem 0 1rem 1rem;
     gap: 1rem;
-    box-shadow: 0 1em 1em -1em hsla(0, 0%, 0%, 0.25);
+    box-shadow: 0 1em 3em -2em hsla(0, 0%, 0%, 0.25);
     svg {
       width: 4rem;
     }
+  }
+  .button-panel {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: transparent;
+    border-radius: 0 0 20px 20px;
+    box-shadow: 0 -1em 5em -1em hsla(0, 0%, 0%, 0.25);
+    img {
+      height: 1.5rem;
+      opacity: 0.8;
+      cursor: pointer;
+      &:hover {
+        transform: scale(1.2);
+        transition: 0.3s;
+      }
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    grid-column-start: 2;
+    grid-column-end: 3;
+    grid-row-start: 2;
+    grid-row-end: 3;
   }
 `;
