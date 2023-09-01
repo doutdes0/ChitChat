9a2b65e0970b8f2aaa51ab6d6e0da625ef8056b4
Artem Gromakov <pandawithmelon@gmail.com>
2023-09-01 19:26:21 +0300

    Pass down selectedCharID, add media query

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,9 +1,10 @@
-import UserPanel from '../components/UserPanel';
-import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../hooks/useRedux';
-import styled from 'styled-components';
-import ChatWindow from '../components/ChatWindow';
-import Welcome from '../components/Welcome';
+import UserPanel from "../components/UserPanel";
+import { useState } from "react";
+import { useAppSelector } from "../hooks/useRedux";
+
+import styled from "styled-components";
+import ChatWindow from "../components/ChatWindow";
+import Welcome from "../components/Welcome";
 
 export interface selectedChat {
   _id: string;
@@ -17,20 +18,18 @@ const Chat: React.FC = () => {
   const id = useAppSelector((state) => state.auth.id);
 
   const [selectedChat, setSelectedChat] = useState<selectedChat | null>(null);
+
   return (
     <Container>
       <UserPanel
         username={username}
         avatar={avatar}
-        id={id}
         setChat={setSelectedChat}
+        selectedChatID={selectedChat?._id}
       />
       <h2>ChitChAt</h2>
       {selectedChat ? (
-        <ChatWindow
-          id={id}
-          selectedChat={selectedChat}
-        />
+        <ChatWindow id={id} selectedChat={selectedChat} />
       ) : (
         <Welcome username={username} />
       )}
@@ -42,19 +41,19 @@ export default Chat;
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
-  background-image: url('/src/assets/main-bg.svg');
+  background-image: url("/src/assets/main-bg.svg");
   /* background by SVGBackgrounds.com */
   background-repeat: no-repeat;
   background-size: cover;
   display: grid;
   grid-template-columns: [first] 10% [line1] 25% [line2] 5% [line3] 50% [line4] 10% [last];
-  grid-template-rows: [first] 10% [row1] auto [row2] 10% [last];
+  grid-template-rows: [first] 10% [row1] 80% [row2] 10% [last];
   & > h2 {
     font-family: var(--ms-family);
-    grid-column-start:3;
+    grid-column-start: 3;
     grid-column-end: 4;
-    grid-row-start:2;
-    grid-row-end:3;
+    grid-row-start: 2;
+    grid-row-end: 3;
     justify-self: center;
     align-self: center;
     writing-mode: vertical-lr;
@@ -62,5 +61,34 @@ const Container = styled.div`
     color: var(--white);
     letter-spacing: 1.5rem;
     opacity: 0.5;
-}
+  }
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    overflow-y: auto;
+    grid-template-columns: [first] 5% [line2] 90% [line3] 5% [last];
+    grid-template-rows: [first] 10% [row2] 70% [row3] 10% [row4] 70% [row5] 10% [last];
+    & > h2 {
+      grid-column-start: 2;
+      grid-column-end: 3;
+      grid-row-start: 3;
+      grid-row-end: 4;
+      writing-mode: initial;
+      text-orientation: initial;
+    }
+    &::-webkit-scrollbar {
+      width: 5px;
+    }
+    &::-webkit-scrollbar-thumb {
+      width: 10px;
+      background: linear-gradient(
+        hsl(164, 60%, 29%, 0.6),
+        hsl(164, 58%, 49%, 0.6)
+      );
+      opacity: 0.1;
+      border-radius: 100px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+  }
 `;
