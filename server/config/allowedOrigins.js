64b1a70ef39516decab6ac480faffbf687e4aaf1
Artem Gromakov <pandawithmelon@gmail.com>
2023-09-09 22:03:52 +0300

    Add github adress to allowed origins

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://192.168.0.54:5173",
+  "https://github.com/doutdes0/ChitChat",
+];
 
 module.exports = allowedOrigins;
