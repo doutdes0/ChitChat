3e39bf6bc66dea1f93098d8d0498252d897352db
Artem Gromakov <pandawithmelon@gmail.com>
2023-09-09 22:34:26 +0300

    Add credentials header

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,14 +1,15 @@
-const allowedOrigins = require('./allowedOrigins');
+const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 204,
+  credentials: true,
 };
 
 module.exports = corsOptions;
