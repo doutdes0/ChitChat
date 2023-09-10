88e2058861083277f34eaf76ba411e9eba85713b
Artem Gromakov <pandawithmelon@gmail.com>
2023-09-10 22:09:10 +0300

    Increase access token life for dev purposes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,14 +1,17 @@
-const bcrypt = require('bcrypt');
-const User = require('../model/userModel');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const User = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   try {
     const { username, password, email } = req.body;
     const unExists = await User.findOne({ username });
-    if (unExists) return res.status(401).json({ msg: 'Username is taken' });
+    if (unExists) return res.status(401).json({ msg: "Username is taken" });
     const emailExists = await User.findOne({ email });
-    if (emailExists) return res.status(401).json({ msg: 'User with this email already exists' });
+    if (emailExists)
+      return res
+        .status(401)
+        .json({ msg: "User with this email already exists" });
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({ username, email, password: hashedPassword });
     res.sendStatus(200);
@@ -21,16 +24,26 @@ const login = async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username }).lean();
-    if (!user) return res.status(401).json({ msg: 'Incorrect username or password' });
+    if (!user)
+      return res.status(401).json({ msg: "Incorrect username or password" });
     const isPwValid = await bcrypt.compare(password, user.password);
-    if (!isPwValid) return res.status(401).json({ msg: 'Incorrect username or password' });
+    if (!isPwValid)
+      return res.status(401).json({ msg: "Incorrect username or password" });
 
-    const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: '30s',
-    });
-    const refreshToken = jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET, {
-      expiresIn: '1d',
-    });
+    const accessToken = jwt.sign(
+      { username },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "3000s",
+      }
+    );
+    const refreshToken = jwt.sign(
+      { username },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
     await User.updateOne({ username }, { refreshToken });
     user._id = user._id.toString();
     user.accessToken = accessToken;
@@ -39,9 +52,9 @@ const login = async (req, res, next) => {
     delete user.refreshToken;
     delete user.__v;
     res
-      .cookie('jwt', refreshToken, {
+      .cookie("jwt", refreshToken, {
         httpOnly: true,
-        sameSite: 'None',
+        sameSite: "None",
         secure: true,
         maxAge: 24 * 60 * 60 * 1000,
       })
@@ -57,23 +70,31 @@ const refreshToken = async (req, res, next) => {
     // console.log('refreshToken contr: ', req.headers);
     const cookies = req.cookies;
     // console.log('refreshC: logging cookie: ', cookies.jwt);
-    if (!cookies.jwt) return res.status(401).json({ msg: 'No refresh token cookie' });
+    if (!cookies.jwt)
+      return res.status(401).json({ msg: "No refresh token cookie" });
     const refreshToken = cookies.jwt;
     const user = await User.findOne({ refreshToken });
-    if (!user) res.status(401).json({ msg: 'User with this refresh token not found' });
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-      if (err || user.username !== decoded.username) {
-        return res.status(401).json({ msg: 'Invalid token(belongs to another user)' });
-      }
-      const accessToken = jwt.sign(
-        { username: decoded.username },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-          expiresIn: '30s',
+    if (!user)
+      res.status(401).json({ msg: "User with this refresh token not found" });
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err || user.username !== decoded.username) {
+          return res
+            .status(401)
+            .json({ msg: "Invalid token(belongs to another user)" });
         }
-      );
-      res.status(200).json({ accessToken });
-    });
+        const accessToken = jwt.sign(
+          { username: decoded.username },
+          process.env.ACCESS_TOKEN_SECRET,
+          {
+            expiresIn: "30s",
+          }
+        );
+        res.status(200).json({ accessToken });
+      }
+    );
   } catch (e) {
     next(e);
   }
@@ -93,7 +114,9 @@ const setAvatar = async (req, res, next) => {
 const getAllUsers = async (req, res, next) => {
   try {
     const username = req.user;
-    const list = await User.find({ username: { $ne: username } }).select('avatar username _id');
+    const list = await User.find({ username: { $ne: username } }).select(
+      "avatar username _id"
+    );
     return res.status(200).json({ list });
   } catch (e) {
     next(e);
